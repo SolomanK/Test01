31b3a0ec86bcc53c91643025284d3f76af44a85b
SolomanK <17688705524@163.com>
2018-01-10 12:54:25 +0800

    first

--- src/test/Git.java
+++ src/test/Git.java
@@ -2,8 +2,8 @@ package test;
 
 public class Git {
     public static void ddd(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
     }
 }
