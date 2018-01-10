12a15d2d082bc4a58567dcd27dc096f96523a3a6
SolomanK <17688705524@163.com>
2018-01-10 12:28:45 +0800

    first

---
+++ src/test/Git.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Git {
+    public static void ddd(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
